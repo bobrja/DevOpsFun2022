888b88fe98920d96c0371c28e2d105561035f74e
Yurii Kovtsun <bobrja@gmail.com>
2022-11-13 18:17:17 +0000

    upd: add comment in task1 file

--- task1.sh
+++ task1.sh
@@ -2,4 +2,7 @@
 cat "${@}"
 # Some comment for branch fixBranch
 # by Yurii Kovtsun
-# upd in 19:39
+# upd in 19:33
+# some update by 13.11.20223
+# some update by 13.11.20223
+# some update by 13.11.20229
